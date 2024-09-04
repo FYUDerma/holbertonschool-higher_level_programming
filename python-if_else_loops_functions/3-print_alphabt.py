8bbb86179a4275b5cb5cfebd68cdde8afe080b68
FYUDerma <zakaria.12ans@gmail.com>
2024-09-04 09:17:17 +0200

    fixed using one print function

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+letters = ""
 for letter in range(97, 122):
     if letter != 113 and letter != 101:
-        print(chr(letter), end='')
+        letters += chr(letter)
+print("{}".format(letters))
