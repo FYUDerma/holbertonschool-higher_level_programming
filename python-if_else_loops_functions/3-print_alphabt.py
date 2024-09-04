a9b540446ef22b4191b6bb51ef01a5e1475c96a1
FYUDerma <zakaria.12ans@gmail.com>
2024-09-04 09:12:30 +0200

    fixed using ascii table

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in "abcdefghijklmnopqrstuvwxyz":
-    print(letter, end='')
-    
+for letter in range(97, 122):
+    if letter != 113 and letter != 101:
+        print(chr(letter), end='')
