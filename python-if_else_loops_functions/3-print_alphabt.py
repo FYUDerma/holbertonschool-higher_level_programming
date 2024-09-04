4041cc1ff796ab4255b10abb5bdbc2a89566a5f5
FYUDerma <zakaria.12ans@gmail.com>
2024-09-04 09:18:08 +0200

    fixed printing the missing character

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 letters = ""
-for letter in range(97, 122):
+for letter in range(97, 123):
     if letter != 113 and letter != 101:
         letters += chr(letter)
 print("{}".format(letters))
