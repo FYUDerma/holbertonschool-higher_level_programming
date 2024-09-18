5a448b943a1df9331391882cfb295ca1d18406e6
FYUDerma <zakaria.12ans@gmail.com>
2024-09-18 10:04:05 +0200

    project 0 to 3 with main file for rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Rectangle"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+
+    It does not have any attributes or methods currently.
+    """
+    pass
