99e98418fed1ac53e7b964f7b97ac1546cfb3b7d
FYUDerma <zakaria.12ans@gmail.com>
2024-09-18 10:01:46 +0200

    fixing printing

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -66,8 +66,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+
+        [print("") for i in range (0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
