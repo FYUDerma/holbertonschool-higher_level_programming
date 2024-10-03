9b59879164de99bc56751f3da4e92a380077ca7b
FYUDerma <zakaria.12ans@gmail.com>
2024-10-03 17:21:09 +0200

    initial commit

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary description of an object for JSON serialization.
+
+        Returns:
+            A dictionary representation of the object's attributes
+        """
+        json_dict = {}
+        for attr in dir(self):
+            if not attr.startswith('__'):  # exclude special attributes
+                value = getattr(self, attr)
+                if isinstance(value, (list, dict, str, int, bool)):
+                    json_dict[attr] = value
+        return json_dict
