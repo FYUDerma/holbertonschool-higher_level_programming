384b966e7e0c037f4fe9b8b51dea0f422f6696a3
FYUDerma <zakaria.12ans@gmail.com>
2024-10-04 11:37:42 +0200

    initial commit

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Learn how to serialize and deserialize
+Custom Python objects using the pickle module.
+"""
+import json
+import pickle
+
+
+class CustomObject:
+    """
+    A custom object to test serialization and deserialization.
+    """
+    def __init__(self, name, age, is_student):
+        """
+        Initialize the custom object.
+        """
+        self.__name__ = name
+        self.__age__ = age
+        self.__is_student__ = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
