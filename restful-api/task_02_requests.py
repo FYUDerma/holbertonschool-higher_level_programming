9e0df511a7e4ba2c0ac61c576ab4d57bbd27ac56
FYUDerma <zakaria.12ans@gmail.com>
2024-10-15 12:26:29 +0200

    test fix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,7 +17,6 @@ def fetch_and_save_posts():
     """Fetchs post from the JSONPlaceHolder."""
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
         data = response.json()
         with open("posts.csv", "w", newline='') as file:
