bf64f921d805156b9d244bd46d5de38c6318d806
FYUDerma <zakaria.12ans@gmail.com>
2024-10-15 17:31:48 +0200

    fixed

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,9 +3,7 @@ import http.server
 import socketserver
 import json
 
-
 PORT = 8000
-
 
 class BasicHttpServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -29,8 +27,7 @@ class BasicHttpServer(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            info = {"version": "1.0", "description":
-                    "A simple API built with http.server"}
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode())
         else:
             self.send_response(404)
@@ -38,7 +35,7 @@ class BasicHttpServer(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"404 Not Found. Endpoint not available.")
 
-
-with socketserver.TCPServer(("", PORT), BasicHttpServer) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), BasicHttpServer) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
