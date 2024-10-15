b6067dea7fc3583d498e3a88b8c889adb47d8efa
FYUDerma <zakaria.12ans@gmail.com>
2024-10-15 12:29:23 +0200

    fix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import requests
+import csv
 
 
+url = "https://jsonplaceholder.typicode.com/posts"
 def fetch_and_print_posts():
     """Fetches posts from the JSONPlaceHolder."""
-    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
@@ -15,9 +16,11 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     """Fetchs post from the JSONPlaceHolder."""
-    url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
+        posts = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in data]
         with open("posts.csv", "w", newline='') as file:
-            file.write(str(data))
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts)
