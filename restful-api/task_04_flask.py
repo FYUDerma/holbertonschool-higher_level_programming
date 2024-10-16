84640fdfa5353aef74cb2e16cd66398da990a0f1
FYUDerma <zakaria.12ans@gmail.com>
2024-10-16 18:18:06 +0200

    Refactor Flask API routes and improve data retrieval

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -21,48 +21,21 @@ users = {
 
 @app.route("/")
 def home():
-    """
-    Returns a welcome message to the Flask API.
-
-    Returns:
-        str: "Welcome to the Flask API!"
-    """
     return "Welcome to the Flask API!"
 
 
 @app.route("/data", methods=['GET'])
 def data():
-    """
-    Returns a list of all user usernames.
-
-    Returns:
-        json: A JSON response containing a list of usernames.
-    """
-    return jsonify(list(users.keys()))
+    return jsonify([user['username'] for user in users.values()])
 
 
 @app.route("/status")
 def status():
-    """
-    Returns the status of the API.
-
-    Returns:
-        str: "OK"
-    """
     return "OK"
 
 
 @app.route("/users/<username>")
 def show_user_profile(username):
-    """
-    Returns the user profile for the given username.
-
-    Args:
-        username (str): The username to retrieve.
-
-    Returns:
-        json: A JSON response containing the user profile if found, otherwise an error message.
-    """
     if username in users:
         return jsonify(users[username])
     else:
@@ -71,15 +44,6 @@ def show_user_profile(username):
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
-    """
-    Adds a new user to the system.
-
-    Args:
-        data (dict): A dictionary containing the user data.
-
-    Returns:
-        json: A JSON response containing a success message and the added user data if successful, otherwise an error message.
-    """
     data = request.get_json()
     if 'username' not in data:
         return jsonify({"error": "Username is required"}), 400
