c562d4a7af7ce16f1216eee8800598fce6de6896
FYUDerma <zakaria.12ans@gmail.com>
2024-10-17 17:40:34 +0200

    fixing pycodestyle

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -15,29 +15,43 @@ jwt = JWTManager(app)
 auth = HTTPBasicAuth()
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+        },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+        }
 }
+
 
 @jwt.unauthorized_loader
 def handle_unauthorized_error(err):
     return jsonify({"error": "Missing or invalid token"}), 401
 
+
 @jwt.invalid_token_loader
 def handle_invalid_token_error(err):
     return jsonify({"error": "Invalid token"}), 401
+
 
 @jwt.expired_token_loader
 def handle_expired_token_error(err):
     return jsonify({"error": "Token has expired"}), 401
 
+
 @jwt.revoked_token_loader
 def handle_revoked_token_error(err):
     return jsonify({"error": "Token has been revoked"}), 401
 
+
 @jwt.needs_fresh_token_loader
 def handle_needs_fresh_token_error(err):
     return jsonify({"error": "Fresh token required"}), 401
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -45,10 +59,12 @@ def verify_password(username, password):
         if check_password_hash(users[username]['password'], password):
             return username
 
+
 @app.route('/basic-protected', methods=['GET'])
 @auth.login_required
 def basic_protected():
     return "Basic Auth: Access Granted"
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -65,19 +81,22 @@ def login():
     else:
         return jsonify({"ERROR": "Bad username or password"}), 401
 
+
 @app.route('/jwt-protected', methods=['GET'])
 @jwt_required()
 def jwt_protected():
     return jsonify({"JWT Auth": "Access Granted"}), 200
+
 
 @app.route("/admin-only", methods=["GET"])
 @jwt_required()
 def admin_only():
     current_user = get_jwt_identity()
     if users[current_user]["role"] == "admin":
-        return jsonify({"Admin Access" : "Granted" }), 200
+        return jsonify({"Admin Access": "Granted"}), 200
     else:
-        return jsonify({"Admin Access" : "Admin access required"}), 401
+        return jsonify({"Admin Access": "Admin access required"}), 401
+
 
 if __name__ == '__main__':
     app.run()
