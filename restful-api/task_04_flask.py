b67634b8883c5aa5897eb42ef6155b1f54c1c17d
FYUDerma <zakaria.12ans@gmail.com>
2024-10-18 11:15:08 +0200

    fixing data layout for post request and error code

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -46,16 +46,26 @@ def show_user_profile(username):
 @app.route("/add_user", methods=['POST'])
 def add_user():
     data = request.get_json()
+    username = data.get('username', None)
+    name = data.get('name', None)
+    age = data.get('age', None)
+    city = data.get('city', None)
 
     if not "username" in data:
         return jsonify({"error": "Username is required"}), 400
 
-    username = data['username']
+    users[username] = {
+        "username": username,
+        "name": name,
+        "age": age,
+        "city": city
+    }
+
     if username in users:
-        return jsonify({'message': 'User  already exists'})
+        return jsonify({'message': 'User  already exists'}), 409
     else:
         users[username] = data
-        return jsonify({'message': 'User  added', 'user': data})
+        return jsonify({'message': 'User  added', 'user': users[username]}), 201
 
 
 if __name__ == "__main__":
