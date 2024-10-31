2c65d46d23fd3138e336472727fb92e4d29ddece
FYUDerma <zakaria.12ans@gmail.com>
2024-10-31 14:49:31 +0100

    Add SQL script to create second_table with score column

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,10 @@
 -- Create a second_table
-CREATE TABLE second_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS second_table
+(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 -- Insert data into second_table
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
